e6e4d03e05871f75830c95b68def56762b1c8161
Zedan <zsalama07@gmail.com>
2021-08-23 18:13:53 +0300

    Co-authored-by: roseline98 <roseline98@users.noreply.github.com>

--- src/app/myaccount/myaccount.page.ts
+++ src/app/myaccount/myaccount.page.ts
@@ -6,6 +6,7 @@ import { FirebaseService } from '../services/firebase.service';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { loggedIn } from '@angular/fire/auth-guard';
+import { isAPIResponseSuccess } from 'ionic';
 
 @Component({
   selector: 'app-myaccount',
@@ -20,11 +21,109 @@ export class MyaccountPage implements OnInit {
     public firebaseAuth: AngularFireAuth,
     public router: Router,
     public FirebaseService: FirebaseService,
-    public fireStore: AngularFirestore
+    public fireStore: AngularFirestore,
+    public fireservices: AngularFirestore
   ) {}
+  public LeftData: Observable<any>;
+  public RightData: Observable<any>;
 
   ngOnInit() {
-    this.Mine = this.fireStore.collection('users').valueChanges();
+    this.LeftData = this.fireservices
+      .collection('OferPark')
+      .doc('Left')
+      .collection('LeftPark')
+      .valueChanges(); //Left Park Data
+    this.RightData = this.fireservices
+      .collection('OferPark')
+      .doc('Right')
+      .collection('RightPark')
+      .valueChanges(); //Right Park Data
+  }
+  async cancelReservation() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Park Cancelation',
+      message: 'Cancel Your Reservation',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Reservation Canceled');
+            return;
+          },
+        },
+        {
+          text: 'Confirm',
+          role: 'confirm',
+          cssClass: 'primary',
+          handler: () => {
+            let currentUserEmail;
+            this.firebaseAuth.user.subscribe((user) => {
+              currentUserEmail = user.email;
+              console.log(currentUserEmail);
+              this.fireservices
+                .collection('users')
+                .doc(currentUserEmail)
+                .get()
+                .subscribe(async (data) => {
+                  let currentDoc: any;
+                  currentDoc = data.data();
+                  console.log(currentDoc);
+                  if (currentDoc.Pos === 'Left') {
+                    this.fireservices
+                      .collection('OferPark')
+                      .doc('Left')
+                      .collection('LeftPark')
+                      .doc(currentDoc.ParkName)
+                      .update({
+                        Status: 'Available',
+                        Color: 'success',
+                        Email: '',
+                      });
+                    if (currentDoc.isParked === true) {
+                      this.fireservices
+                        .collection('users')
+                        .doc(currentUserEmail)
+                        .update({
+                          isParked: false,
+                          ParkName: '',
+                          Side: '',
+                        });
+                    }
+                  } else {
+                    this.fireservices
+                      .collection('OferPark')
+                      .doc('Right')
+                      .collection('RightPark')
+                      .doc(currentDoc.ParkName)
+                      .update({
+                        Status: 'Available',
+                        Color: 'success',
+                        Email: '',
+                      });
+                    if (currentDoc.isParked === true) {
+                      this.fireservices
+                        .collection('users')
+                        .doc(currentUserEmail)
+                        .update({
+                          isParked: false,
+                          ParkName: '',
+                          Side: '',
+                        });
+                    }
+                  }
+                });
+            });
+            return;
+          },
+        },
+      ],
+    });
+    await alert.present();
+    const result = await alert.onDidDismiss();
+    console.log(result);
   }
 
   async logout() {
