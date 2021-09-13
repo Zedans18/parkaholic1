da9fab1ae9d5296c331c75b0355a378572a313b3
roseline <ammarroseline@gmail.com>
2021-09-13 16:16:53 +0300

    ayooo

--- src/app/first/first.page.ts
+++ src/app/first/first.page.ts
@@ -8,6 +8,7 @@ import { Observable, timer } from 'rxjs';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { interval } from 'rxjs';
 import { DOCUMENT } from '@angular/common';
+import { ParkService } from '../park.service';
 
 @Component({
   selector: 'app-first',
@@ -31,7 +32,8 @@ export class FirstPage implements OnInit {
     private router: Router,
     public toaster: ToastController,
     public platform: Platform,
-    @Inject(DOCUMENT) public _document: Document
+    @Inject(DOCUMENT) public _document: Document,
+    public parkService: ParkService
   ) {}
 
   ngOnInit() {
@@ -87,86 +89,7 @@ export class FirstPage implements OnInit {
           });
         });
     });
-
-    this.firebaseAuth.user.subscribe((user) => {
-      //This function checks if the user's park has been taken by someone else.
-      //Then it changes the park's info.
-      currentUserEmail = user.email;
-      console.log(currentUserEmail);
-      this.fireservices
-        .collection('OferPark')
-        .doc('Left')
-        .collection('LeftPark')
-        .valueChanges()
-        .subscribe((data) => {
-          data.forEach(async (value) => {
-            if (value.Status === 'Reserved') {
-              let spot: any;
-              let newPark: any;
-              this.fireservices
-                .collection('users')
-                .doc(currentUserEmail)
-                .update({
-                  isParked: false,
-                  ParkName: '',
-                  Side: '',
-                });
-              this.fireservices
-                .collection('OferPark')
-                .doc('Left')
-                .collection('LeftPark')
-                .valueChanges()
-                .subscribe((data) => {
-                  data
-                    .slice()
-                    .reverse()
-                    .forEach((value) => {
-                      console.log(value);
-                      if (value.Status === 'Available') {
-                        spot = value.ID;
-                        newPark = value.ParkName;
-                      }
-                      return;
-                    });
-                });
-
-              this.checkTime.subscribe(async (time) => {
-                //If someone who doesn't own the app parked in the user's spot.
-                //The app automatically reserve for the user the first empty spot.
-                if (time === 5) {
-                  this.fireservices
-                    .collection('OferPark')
-                    .doc('Left')
-                    .collection('LeftPark')
-                    .doc(value.ParkName)
-                    .update({
-                      Status: 'Taken',
-                    });
-                  this.fireservices
-                    .collection('OferPark')
-                    .doc('Left')
-                    .collection('LeftPark')
-                    .doc(newPark)
-                    .update({
-                      Status: 'Pending',
-                      Color: 'warning',
-                      Email: currentUserEmail,
-                      Time: new Date().getMinutes(),
-                    });
-                  this.fireservices
-                    .collection('users')
-                    .doc(currentUserEmail)
-                    .update({
-                      isParked: true,
-                      ParkName: newPark,
-                      Side: 'Left',
-                    });
-                }
-              });
-            }
-          });
-        });
-    });
+    this.parkService.checkPark();
   }
 
   async presentAlertConfirmDisability(park) {
